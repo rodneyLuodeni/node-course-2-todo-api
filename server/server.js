d9467a6fddea75e3cb119c700dbcbdaaffa68426
Rodney Wormsbecher <rodney22@live.nl>
2017-12-10 00:49:55 +0100

    Created the project

---
+++ server/server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var {mongoose } = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    var todo = new Todo({
+       text: request.body.text
+    });
+
+    todo.save().then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response.status(400).send(error);
+    })
+});
+
+
+//GET /todos
+
+app.listen(3000, () => {
+    console.log('started on port 3000');
+});
