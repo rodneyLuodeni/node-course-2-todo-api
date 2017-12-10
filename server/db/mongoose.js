b52706d8bc31bd6ef65fe33cea3409adbe922fe8
Rodney Wormsbecher <rodney22@live.nl>
2017-12-11 00:38:04 +0100

    updated login info

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://Rodney:php123@ds135956.mlab.com:35956/todoapp-mongo-database');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+
 
 
 
