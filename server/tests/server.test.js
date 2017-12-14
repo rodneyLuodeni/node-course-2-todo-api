2bf53775f763c418f8e867f4fe290270017708a9
Rodney Wormsbecher <rodney22@live.nl>
2017-12-14 16:03:15 +0100

    Added tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -9,7 +9,9 @@ const {Todo} = require('./../models/todo');
 const dummyTodos = [
     {
         _id: new ObjectID(),
-        text: 'First test todo'
+        text: 'First test todo',
+        completed: true,
+        completedAt: 333
     },
     {
         _id: new ObjectID(),
@@ -162,4 +164,52 @@ describe('GET /todo/:id', () => {
                     done();
                 });
         });
-    })
+    });
+
+
+describe('PATCH /todos/:id', () => {
+
+    it('Should update the todo', (done) => {
+        var hexId = dummyTodos[1]._id.toHexString();
+        var text = 'This should be the new text';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send( {
+                completed: true,
+                text: text
+            })
+            .expect(200)
+            .expect( (response) => {
+                expect(response.body.todo.text).toBe(text);
+                expect(response.body.todo.completed).toBe(true);
+         
+            })            
+            .end(done);
+    });
+
+    it("Should cleare the completedAt when todo is not completed", (done) => {
+        var hexId = dummyTodos[0]._id.toHexString();
+        var text = 'This should be the new text';
+
+        request(app)
+            .patch(`/todos/${hexId}`)
+            .send({
+                completed: false,
+                completedAt: null,
+                text
+            })
+            .expect(200)
+            .expect( (response) => {
+                expect(response.body.todo.completedAt).toBe(null)
+            })
+            .end(done);
+
+    });
+
+})
+
+
+
+
+
